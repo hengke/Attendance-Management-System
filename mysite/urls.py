98f4bb922cd0afeef8a6dea5809907afe829ec51
ZhengHengKe <zhenghengke@qq..com>
2019-05-16 23:00:34 +0800

    设置虚拟环境

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-from  app import views
+from app import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',views.index),
